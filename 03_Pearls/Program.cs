6bc29bb049aba22731d8a9b00bac3924b3a2630f
Martin <martin_lenart@icloud.com>
2023-09-28 16:33:42 +0200

    update pearls

--- 03_Pearls/Program.cs
+++ 03_Pearls/Program.cs
@@ -1,10 +1,85 @@
-﻿namespace _03_Pearls;
+﻿using System.Diagnostics;
+using Helpers;
+
+namespace _03_Pearls;
+
+public enum enPearlColor { Black, White, Pink }
+public enum enPearlShape { Round, DropShaped }
+public enum enPearlType { FreshWater, SaltWater }
+
+public class csPearl
+{
+    public const int PearlMinSize = 5;
+    public const int PearlMaxSize = 25;
+
+    public enPearlColor Color { get;  }
+    public enPearlShape Shape { get;  }
+    public enPearlType Type { get;  }
+
+    int _size;
+    public int Size
+    {
+        get => _size;
+        set
+        {
+            if (value < PearlMinSize || value > PearlMaxSize)
+                throw new ArgumentOutOfRangeException(nameof(Size));
+            _size = value;
+        }
+    }
+
+    public override string ToString() => $"{Size}mm {Color} {Shape} {Type} pearl.";
+
+    public csPearl()
+    {
+        var _seeder = new csSeedGenerator();
+        Size = _seeder.Next(PearlMinSize, PearlMaxSize + 1);
+        Color = _seeder.FromEnum<enPearlColor>();
+        Shape = _seeder.FromEnum<enPearlShape>();
+        Type = _seeder.FromEnum<enPearlType>();
+    }
+}
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Console.WriteLine("Hello, Perls!");
+
+        //Create a necklace
+        csPearl[] necklace = new csPearl[10];
+        for (int i = 0; i < 10; i++)
+        {
+            necklace[i] = new csPearl();
+        }
+
+        //Find min and max pear
+        int maxSize = int.MinValue;
+        int maxIdx = 0;
+        int minSize = int.MaxValue;
+        int minIdx = 0;
+        for (int i = 0; i < 10; i++)
+        {
+            if (necklace[i].Size > maxSize)
+            {
+                maxSize = necklace[i].Size;
+                maxIdx = i;
+            }
+            if (necklace[i].Size < minSize)
+            {
+                minSize = necklace[i].Size;
+                minIdx = i;
+            }
+        }
+
+        //Present it to the user
+        Console.WriteLine("\nThis is the neclace");
+        foreach (var item in necklace)
+        {
+            Console.WriteLine(item);
+        }
+        Console.WriteLine($"Largest pearl: {necklace[maxIdx]}");
+        Console.WriteLine($"Smalest pearl: {necklace[minIdx]}");
     }
 }
 
